8c14e3294bfd268375a3aff94775c2833ac7d9f1
filwie <filip.wiechec@gmail.com>
2019-09-23 21:41:41 +0200

    Specify description type

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(name='',
       version='0.0.1',
       description='',
       long_description=readme(),
+      long_description_content_type='text/markdown',
       classifiers=[
           'Programming Language :: Python :: 3',
           'License :: OSI Approved :: MIT License',
